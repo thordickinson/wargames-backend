65e71d6f9de0add256213af83dda716515567b30
Dickinson Arismendy <thordickinson@gmail.com>
2024-08-28 11:37:34 -0500

    first version

---
+++ README.md
@@ -0,0 +1,16 @@
+Project Overview and Technology List
+Context: This project involves developing a system to manage war game planning sessions. The system will consist of a backend server, an administrative frontend, and a client application. The primary goal is to create a robust environment where users can join and participate in game planning sessions, record their discussions, and analyze the results. The administrative interface will allow facilitators to create, manage, and review these sessions.
+
+Communication Protocol:
+
+GraphQL with Apollo Server: We will use Apollo Server with GraphQL as the communication protocol between the frontend and backend. This allows for efficient querying of data, ensuring that clients can request exactly what they need and nothing more.
+Technologies:
+
+Node.js
+TypeScript
+GraphQL (Apollo Server)
+MongoDB
+Mongoose
+RabbitMQ
+This list specifies the core technologies we will be using for this project, ensuring that all parts of the system are interconnected and can operate smoothly.
+
---
+++ src/sample_data.js
@@ -0,0 +1,36 @@
+// This file describes the sample data used in the demo.
+
+
+const GamesCollection = [
+    {   
+        key: "malign", 
+        name: "Malign", description: "Un juego a cerca de guerra y comunicaciones", 
+        teams: [
+        {key: "ursavia", name: "Ursavia", description: "Un equipo que debe destruir todos los aliados"},
+        {key: "ukriv", name: "Ukriv", description: "Todos están en contra de ellos"},
+        {key: "islive", name: "Islive", description: "Una nación rica y neutral"},
+        {key: "frankia", name: "Frankia", description: "Un neutro que siempre ha estado en contra de ursavia"}
+        ],
+        roles: [
+            {key: "fuegos", name: "Fuegos", description: "Encargados de proteger la nación"},
+            {key: "proteccion", name: "Protección", description: "Encargados de la protección de la nación"},
+            {key: "comando_control", name: "Comando y Control", description: "Cabeza de operaciones"},
+            {key: "movimiento_maniobra", name: "Movimiento y Maniobra", description: "Encargados de la movilidad de la nación"},
+            {key: "sostenimiento", name: "Sostenimiento", description: "Encargados de la sostenibilidad de la nación"},
+            {key: "informacion", name: "Información", description: "Inteligencia y manejo de sistemas de información"},
+            {key: "inteligencia", name: "Inteligencia", description: "Espionaje y manejo de sistemas de información"},
+        ]
+    },
+]
+
+
+const GameSessionCollection = [
+    {
+        _id: "",
+        createdAt: "2024-08-28T16:34:21Z",
+        status: "created", /* created, started, ended */
+        gameId: "", /* ref to Games Collection */
+
+    }
+]
+

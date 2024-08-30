46f91f2b879c728c21af3628c782b1ca58ef3a2c
Dickinson Arismendy <thordickinson@gmail.com>
2024-08-30 10:54:06 -0500

    feat: loading dotenv files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
+import 'dotenv/config'
+
 import { ApolloServer } from 'apollo-server';
 import mongoose from 'mongoose';
 import typeDefs from './schema/schema';
 import resolvers from './resolvers/resolvers';
 
+const MONGO_URL = process.env.MONGO_URL ?? 'mongodb://localhost:27017/wargames';
+
 const startServer = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    await mongoose.connect(MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
